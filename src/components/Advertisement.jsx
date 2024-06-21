9cc86417e5487312eb50e26e9aa207f4be4238d5
karanjiShiva1 <shivashankarkaranji@gmail.com>
2024-06-21 17:45:51 +0530

    commit

--- src/components/Advertisement.jsx
+++ src/components/Advertisement.jsx
@@ -3,41 +3,43 @@ import './advert.css'
 
 function Advertisement() {
   return (
-    <div className='image-container ml-0'>
+    <div className='mt-6 image-container ml-0'>
       <div className=" w-screen flex gap-16 bg-slate-500 p-3 overflow-hidden ">
-      <img className="w-72 h-20" 
+      <img className=" w-72 h-20" 
       src="https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg"
       />
      <img 
+     className=" w-72 h-20"
+     src=
+     "https://img.freepik.com/free-photo/book-composition-with-open-book_23-2147690555.jpg"
+     />
+     <img 
      className="w-72 h-20"
      src=
-     "https://plus.unsplash.com/premium_photo-1667055670847-14e0dbe20ed5?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1pbi1zYW1lLXNlcmllc3wyfHx8ZW58MHx8fHx8"
+     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFhtziUi-Hm8BPQx0vCwFhwwXRhktkfdpVDA&s"
      />
      <img 
-     className="w-60 h-20"
-     src=
-     "https://plus.unsplash.com/premium_photo-1667055670847-14e0dbe20ed5?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1pbi1zYW1lLXNlcmllc3wyfHx8ZW58MHx8fHx8"
+     className="w-72 h-20"
+      src=
+      "https://img.freepik.com/premium-photo/big-tree-isolated_34152-2693.jpg"
+      
+
      />
      <img 
-     className="w-60 h-20"
+     className="w-72 h-20"
      src=
-     "https://plus.unsplash.com/premium_photo-1667055670847-14e0dbe20ed5?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1pbi1zYW1lLXNlcmllc3wyfHx8ZW58MHx8fHx8"
+     "https://unarbrepourmonquartier.org/wp-content/uploads/2021/08/Prunus-avium-Lapins_Arbre_rootsplants.co_.uk_.jpg"
      />
      <img 
-     className="w-60 h-20"
+     className=" w-72 h-20"
      src=
      "https://plus.unsplash.com/premium_photo-1667055670847-14e0dbe20ed5?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1pbi1zYW1lLXNlcmllc3wyfHx8ZW58MHx8fHx8"
      />
-     <img 
-     className="w-60 h-20"
+      <img className= " w-72 h-20"
      src=
      "https://plus.unsplash.com/premium_photo-1667055670847-14e0dbe20ed5?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1pbi1zYW1lLXNlcmllc3wyfHx8ZW58MHx8fHx8"
      />
-      <img className="w-60 h-20"
-     src=
-     "https://plus.unsplash.com/premium_photo-1667055670847-14e0dbe20ed5?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1pbi1zYW1lLXNlcmllc3wyfHx8ZW58MHx8fHx8"
-     />
-     <img className="w-72 h-20"
+     <img className=" w-72 h-20"
      src=
      "https://plus.unsplash.com/premium_photo-1667055670847-14e0dbe20ed5?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1pbi1zYW1lLXNlcmllc3wyfHx8ZW58MHx8fHx8"
      />
